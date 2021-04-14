845ab23be93ff8f96d6adf36ff688a3a30676697
Eduardo <eduardo.behr@hotmail.com>
2021-04-14 16:32:04 -0300

    Sender & receiver syncronized with a queue of size 10

--- main/freeRTOS_test.c
+++ main/freeRTOS_test.c
@@ -1,68 +1,82 @@
-#include <stdio.h>
+#include <stdio.h>				// printf
+#include <stdlib.h>  			// rand
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/queue.h"
 
-/* TODO
- * Task 1: generates a random number every 200ms and stores it in the queue
- * Task 2: receives the numbers from the queue every 500ms
+/* Code description: 
+ * Task 1: generates a random number every SENDER_PERIOD [miliseconds] and sends it to the queue
+ * Task 2: receives the numbers from the queue every RECEIVER_PERIOD [miliseconds]
  * ALERT: each task should print the sent/received numbers to serial!
- * 
  */
 
+// definitions
 #define STACK_SIZE 2000
+#define QUEUE_TIMEOUT 50/portTICK_PERIOD_MS
+#define SENDER_PERIOD 100 / portTICK_PERIOD_MS
+#define RECEIVER_PERIOD 100 / portTICK_PERIOD_MS
 
-// task functions
-void senderFunction(){
-	int i = 0;
-	for( ; ; ){
-		printf("Sender Task: %d\n", i);
-		vTaskDelay(200 / portTICK_PERIOD_MS);
-	}
-}
+// Prototypes
+void senderFunction();
+void receiverFunction();
 
-void receiverFunction(){
-	int i = 0;
-	for( ; ; ){
-		printf("Receiver Task: %d\n", i);
-		vTaskDelay(500 / portTICK_PERIOD_MS);
-	}
-}
-
-
-// task handlers
+// Declarations
+QueueHandle_t xQueue;
 TaskHandle_t xSenderTask_h = NULL;
 TaskHandle_t xReceiverTask_h = NULL;
 
-/* Buffers that the tasks being created will use as its stack.  Note this is
- an array of StackType_t variables.  The size of StackType_t is dependent on
-the RTOS port.*/
-// StackType_t xSenderStack[ STACK_SIZE ];
-// StackType_t xReceiverStack[ STACK_SIZE ];
-
-// Structure that will hold the TCB of the task being created.
-// StaticTask_t xSenderBuffer;
-// StaticTask_t xReceiverBuffer;
-
-// This must exist for the life time of the task (docs)
+// These must exist for the life time of the task (docs)
 static uint8_t ucParameterSender;
 static uint8_t ucParameterReceiver;
 
+
+
+
+
 void app_main(void)
 {
+	xQueue = xQueueCreate(10, sizeof(uint32_t));
+	
 	xTaskCreate(&senderFunction, "Sender", STACK_SIZE, &ucParameterSender, tskIDLE_PRIORITY, xSenderTask_h);
 	
 	xTaskCreate(&receiverFunction, "Receiver", STACK_SIZE, &ucParameterReceiver, tskIDLE_PRIORITY, xReceiverTask_h);
 	
-// 	configASSERT(xSenderTask_h);
-// 	configASSERT(xReceiverTask_h);
-	
-	
-	
 	// continues to infinite loop in main task
-    int i = 0;
+    uint32_t i = 0;
     while (1) {
         printf("[%d] Hello world!\n", i);
+		printf("Queue msgs:\t%d\n", uxQueueMessagesWaiting(xQueue));
         i++;
-        vTaskDelay(5000 / portTICK_PERIOD_MS);
+        vTaskDelay(10000 / portTICK_PERIOD_MS);
     }
+}
+
+// task functions
+void senderFunction(){
+	uint32_t i;
+	for( ; ; ){
+		i = rand()%100;
+		// TODO: send value to queue
+		BaseType_t sndStatus = xQueueSend(xQueue, &i, QUEUE_TIMEOUT);
+		if( sndStatus == errQUEUE_FULL ){
+			printf("Queue is full. Dropping %d\n", i);
+		}
+		printf("Sender Task:  \t%d \t @ %d\n", i, xTaskGetTickCount());
+		vTaskDelay(SENDER_PERIOD);
+	}
+}
+
+void receiverFunction(){
+	int i;
+	for( ; ; ){
+// 		i = rand()%100; //TODO: receive value from queue
+		BaseType_t rcvStatus = xQueueReceive(xQueue, &i, QUEUE_TIMEOUT);
+		if(rcvStatus == pdTRUE){
+			printf("Receiver Task:\t%d \t @ %d\n", i, xTaskGetTickCount());
+		}
+		else{
+			printf("Failed to receive message: Queue empty\n");
+		}
+		vTaskDelay(RECEIVER_PERIOD);
+	}
 }
